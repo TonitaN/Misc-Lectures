03dccaa925523b5cdb304367f3cc5ec7f04d51ca
TonitaN <a_nevod@mail.ru>
2023-07-02 23:55:22 +0300

    Refal-Pygments started
    For 1st day

---
+++ SummerPSI/Refal-pygments/refal-pygment.py
@@ -0,0 +1,63 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+class RefalLexer(RegexLexer):
+    name = 'Refal'
+    aliases = ['ref']
+    filenames = ['*.ref']
+    KEYWORDS = [] 
+    def get_tokens_unprocessed(self, text):
+        for index, token, value in super(RefalLexer,self).get_tokens_unprocessed(text):
+            if token is Name and value in self.KEYWORDS:
+                yield index, Keyword, value
+            else:
+                yield index, token, value
+    tokens = {
+        'root': [
+            (r'(\$ENTRY|\$EXTERN)(\s+)', bygroups(Name.Decorator,Whitespace)),
+            (r'\*.*?$', Comment.Singleline),
+            (r'([A-Z][a-zA-Z0-9]*)(\s*)(\{)', 
+             bygroups(Name.Function,Whitespace,Punctuation), 'lhs'),
+            (r'[\s]+', Whitespace),
+            (r'/\*', Comment.Multiline, 'comment')
+        ],
+        'comment': [
+            (r'[^\*/]+', Comment.Multiline),
+            (r'\*/', Comment.Multiline, '#pop')
+        ],
+        'lhs': [
+            (r'\}', Punctuation, '#pop'),
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'[a-zA-Z0-9]+', Name.Constant),
+            (r'\s+', Whitespace),
+            (r'=', Punctuation, 'rhs')
+        ],
+        'rhs': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), 'fun'),
+            (r'[a-zA-Z0-9]+', Name.Constant),
+            (r'\s+', Whitespace),
+            (r';', Punctuation, '#pop')
+        ],
+        'fun': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r'>', Punctuation, '#pop'),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), '#push'),
+            (r'[a-zA-Z0-9]+', Name.Constant),
+            (r'\s+', Whitespace)
+        ],
+        'string': [
+            (r'\\.', String.Single),
+            (r"'", String.Single, '#pop'),
+            (r"[^'\\]", String.Single)
+        ]
+    }
---
+++ SummerPSI/Refal-pygments/test.sh
@@ -0,0 +1,2 @@
+python -m pygments -x -l refal-pygment.py:RefalLexer $1
+read -s -n 1
