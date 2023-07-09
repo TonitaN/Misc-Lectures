defcec51f2f81b0f6917e96e222bc5fe93153dea
TonitaN <a_nevod@mail.ru>
2023-07-09 21:42:01 +0300

    Add raw expressions in lexer
    Это не красиво и содержит много пасты из-за введения параметров, но хотя бы работает...

--- SummerPSI/Refal-pygments/refal-pygment.py
+++ SummerPSI/Refal-pygments/refal-pygment.py
@@ -18,7 +18,8 @@ class RefalLexer(RegexLexer):
             (r'(\$ENTRY)?(\s*)([A-Z][a-zA-Z0-9]*)(\s*)(\{)', 
              bygroups(Name.Decorator,Whitespace,Name.Function,Whitespace,Punctuation), 'body'),
             (r'[\s]+', Whitespace),
-            (r'/\*', Comment.Multiline, 'comment')
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r'(?=.)', Whitespace, 'bare_expr')
         ],
         'body': [
             (r'\}', Punctuation, '#pop'),
@@ -90,6 +91,20 @@ class RefalLexer(RegexLexer):
             (r'\s+', Whitespace),
             (r';', Punctuation, '#pop')
         ],
+        'bare_expr': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r"\(", Punctuation, 'paren-expr'),
+            (r'([1-9][0-9]*|0)', Number.Integer),
+            (r'(#[ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), 'fun-expr'),
+            (r'[a-zA-Z][a-zA-Z0-9]*', Name.Constant),
+            (r'\s+', Whitespace),
+            (r'$', Whitespace, '#pop')
+        ],
         'paren-rhs': [
             (r'/\*', Comment.Multiline, 'comment'),
             (r"'", String.Single, 'string'),
@@ -108,6 +123,34 @@ class RefalLexer(RegexLexer):
             (r'>', Punctuation, '#pop'),
             (r"\(", Punctuation, 'paren-rhs'),
             (r'([1-9][0-9]*|0)', Number.Integer),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), '#push'),
+            (r'[a-zA-Z][a-zA-Z0-9-_]*', Name.Constant),
+            (r'\s+', Whitespace)
+        ],
+        'paren-expr': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r"\(", Punctuation, '#push'),
+            (r'([1-9][0-9]*|0)', Number.Integer),
+            (r'(#[ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'([ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
+            (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), 'fun'),
+            (r'[a-zA-Z][a-zA-Z0-9-_]*', Name.Constant),
+            (r'\s+', Whitespace),
+            (r'\)', Punctuation, '#pop')
+        ],
+        'fun-expr': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r"'", String.Single, 'string'),
+            (r'>', Punctuation, '#pop'),
+            (r"\(", Punctuation, 'paren-rhs'),
+            (r'([1-9][0-9]*|0)', Number.Integer),
+            (r'(#[ets]\.)([a-zA-Z0-9_-]+)',
+             bygroups(Keyword.Reserved,Name.Variable)),
             (r'([ets]\.)([a-zA-Z0-9_-]+)',
              bygroups(Keyword.Reserved,Name.Variable)),
             (r'(<)([A-Z][a-zA-Z0-9]*)(?=[<>\s])', bygroups(Punctuation,Name.Function), '#push'),
