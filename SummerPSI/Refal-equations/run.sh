02c8e8154bbb0cb4db6ad661b17d8afce7cea407
Ilya Afanasyev <affeeal@yandex.ru>
2023-07-14 21:51:38 +0300

    Разделение GenEq и InteractiveJez

--- SummerPSI/Refal-equations/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# REFC and REFGO store paths to the compilator and interpreter respectively
-$REFC Gen_Eq.ref && $REFGO Gen_Eq.rsl;
-$REFC Check_Eq.ref && $REFGO Check_Eq.rsl;
-
--- SummerPSI/Refal-equations/run_HOF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-refgo Preprocess_Inline $1
-Str=`cat temp`
-refc $Str'.ref'
-refgo $Str'+Auxiliaries'
-rm temp
-read -s -n 1
